56d38c2e797ed522d54d121fe23a4b6817593e21
DhruvDiwan <dhruv18031@iiitd.ac.in>
2019-05-07 16:01:05 +0530

    Second functional test

--- functional_tests.py
+++ functional_tests.py
@@ -3,4 +3,5 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+assert 'To-do' in browser.title , "browser title was " + browser.title
+browser.quit()
