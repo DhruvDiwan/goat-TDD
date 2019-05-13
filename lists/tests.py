0b233608f1b21556ba31d5f85370624a4fa93358
DhruvDiwan <dhruv18031@iiitd.ac.in>
2019-05-13 22:06:11 +0530

    added new ORM test class in tests .py , pg 66

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from lists.item import Item
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -41,3 +42,23 @@ class HomePageTest(TestCase):
 		response = self.client.post('/' , data = {'item_text' : 'A new list item'})
 		self.assertIn('A new list item' , response.content.decode())
 		self.assertTemplateUsed(response , 'home.html')
+
+
+		class  ItemModelTest(TestCase):
+
+			def test_saving_and_retrieving_items(self):
+				first_item = Item()
+				first_item.text = "text 1"
+				first_item.save()
+
+				second_item = Item()
+				second_item.text = "text 2"
+				second_item.save()
+
+				saved_items = Item.objects.all()
+				self.assertEqual(saved_items.count() , 2)
+
+				item1 = saved_items[0]
+				item2 = saved_items[1]
+				self.assertEqual(item1 , "text 1")
+				self.assertEqual(item2 , "text 2")
