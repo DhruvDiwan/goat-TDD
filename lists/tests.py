b35bb335354f5eebdbefba3d8eaa8ad6f004f4b8
DhruvDiwan <dhruv18031@iiitd.ac.in>
2019-05-16 00:53:04 +0530

    new model done, pg 118

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -19,18 +19,27 @@ class HomePageTest(TestCase):
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response , 'home.html')
-		
 
-class  ItemModelTest(TestCase):
+
+class  ListAndItemModelTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = "text 1"
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = "text 2"
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list , list_)
+
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count() , 2)
@@ -38,7 +47,9 @@ class  ItemModelTest(TestCase):
 		item1 = saved_items[0]
 		item2 = saved_items[1]
 		self.assertEqual(item1.text , "text 1")
+		self.assertEqual(item1.list , list_)
 		self.assertEqual(item2.text , "text 2")
+		self.assertEqual(item2.list , list_)
 
 class ListViewTest(TestCase):
 
@@ -47,8 +58,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response , 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='itemey1')
-		Item.objects.create(text='itemey2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey1' , list=list_)
+		Item.objects.create(text='itemey2' , list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 
