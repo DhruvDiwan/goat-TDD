3e6da4a1a790d45c7ab2e13e9aa10079dc905c68
DhruvDiwan <dhruv18031@iiitd.ac.in>
2019-05-16 01:08:05 +0530

    pg 121

--- lists/views.py
+++ lists/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from lists.models import Item , List
 
-def home_page(request):
+def home_page(request ):
 	return render(request , 'home.html')
 
 	items = Item.objects.all()
 	return render(request , 'home.html' , {'items' : items})
 
-def view_list(request):
-	items = Item.objects.all()
+def view_list(request , list_id):
+	list_ = List.objects.get(id=list_id)
+	items = Item.objects.filter(list=list_)
 	return render(request , 'list.html' , {'items' : items})
 
 def new_list(request):
