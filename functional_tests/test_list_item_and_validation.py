f4d48b0a8d4ae93476e5211fa51680b7f9587c52
Dhruv Diwan <dhruv18031@iiitd.ac.in>
2019-06-11 16:13:06 +0530

    Seperated FT's into different files

---
+++ functional_tests/test_list_item_and_validation.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.keys import Keys
+from unittest import skip
+from .base import FunctionalTest
+
+class ItemValidationTest(FunctionalTest):
+	@skip
+	def test_cannot_add_empty_lists(self):
+		self.fail('write me')
